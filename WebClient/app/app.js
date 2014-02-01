bd3d844ef781c090abe7ad86f25c140d5cc1d50b
okoledov <alexander.okoledov@gmail.com>
2014-02-01 23:07:52 +0400

    Добавляю проект Visual Studio

---
+++ WebClient/app/app.js
@@ -0,0 +1,42 @@
+﻿/// <reference path="../Scripts/angular-1.1.4.js" />
+
+/*#######################################################################
+  
+  Dan Wahlin
+  http://twitter.com/DanWahlin
+  http://weblogs.asp.net/dwahlin
+  http://pluralsight.com/training/Authors/Details/dan-wahlin
+
+  Normally like to break AngularJS apps into the following folder structure
+  at a minimum:
+
+  /app
+      /controllers      
+      /directives
+      /services
+      /partials
+      /views
+
+  #######################################################################*/
+
+var app = angular.module('customersApp', []);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/CreateOrder',
+            {
+                controller: 'CreateOrder',
+                templateUrl: '/app/partials/createOrder.html'
+            })
+        .when('/login',
+            {
+                controller: 'CreateOrder',
+                templateUrl: '/app/partials/loginForm.html'
+            })
+        .otherwise({ redirectTo: '/login' });
+});
+
+
+
+
---
+++ WebClient/app/services/clientsService.js
@@ -0,0 +1,54 @@
+﻿//This handles retrieving data and is used by controllers. 3 options (server, factory, provider) with 
+//each doing the same thing just structuring the functions/data differently.
+app.service('clientsService', function () {
+	this.getClients = function () {
+		return clients;
+	};
+	this.getDefaultClient = function () {
+	    return clients[0];
+	}
+	this.getHoldings = function () {
+	    return holdings;
+	}
+	this.getDefaultHolding = function () {
+	    return holdings[0];
+	}
+
+    /*
+	this.getClient = function (Id) {
+		for (var i = 0; i < users.length; i++) {
+			if (users[i].id === Id) {
+			    return users[i];
+			}
+		}
+		return null;
+	};
+
+	// ID пользователя, который сейчас залогинен в систему
+	this.getCurrentUserId = function () {
+		return 1;
+	}
+    */
+
+	var holdings = [
+        {
+            Name: "", Id: -1
+        },
+        {
+            Name: "Mail.ru Group", Id: 0
+        }   
+	];
+
+	var clients = [
+        {
+            Name: "Новый клиент", Id: -1, Holding: holdings[0]
+        },
+        {
+        	Name: "Билайн", Id: 0, Holding: holdings[1]
+        },
+        {
+            Name: "МТС", Id: 1, Holding: holdings[0]
+        }
+	];
+
+});
